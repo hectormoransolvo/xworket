660a70055cfb899921ccb92c5e6ed95a578493e9
hectormoransolvo <72460687+hectormoransolvo@users.noreply.github.com>
2021-12-20 19:26:09 -0300

    Delete configapp.js

--- src/configapp.js
+++
@@ -1,4 +0,0 @@
-export const apiv ="https://integrator.magneto365.com/api/v2/vacants?dateRange="
-export const apit ="n3UFDV45SZxA3JuYzu3okVGjSxJo2MEGfWZctpmAY7fW5mqtvz"
-export const apiu ="api.solvoglobal@magneto365.com"
-export const apic ="https://integrator.magneto365.com/api/v2/candidates?vacant="
